f796e7dc6da72aa2b4ac00bb3fedb93595a9a9af
Ilya <49621122+SoupWithWater@users.noreply.github.com>
2024-09-28 22:09:12 +0300

    v 0.16 boroFarm.ino

--- boroFarm.ino
+++ boroFarm.ino
@@ -1,4 +1,4 @@
-//BoroFarm v0.14  Ilya Borodin(c)
+//BoroFarm v0.16  Ilya Borodin(c)
 //0.001  - сбор данных с датчиков
 //0.01 - добавлен вывод данных на экран
 //0.02 - фиксы
@@ -7,6 +7,8 @@
 //0.12 - рефакторинг, чистка легаси кода
 //0.13 - оптимизация кода
 //0.14 - загрузочный экран
+//0.15 - переход c delay() на millis для оптимального управления системой
+//0.16 - управление подсветкой lcd экрана 
 //0.20 - алгоритм мониторинга для полива
 
 #include <DHT.h>
@@ -20,10 +22,11 @@ const int lightIndicatorPin = 0;  //A0 фоторезистор
 const int soilMoisturePin = 1;    //A1 датчик влажности почвы
 #define DHTPIN 2                  //D2 Датчик DHT11
 #define DHTTYPE DHT11             //   Используемый тип датчика DHT11
-const int pumpPin = 5;            //D5 насос  (реле 1)
-const int fanPin = 6;             //D6 вентилятор  (реле 2)
-const int lightPin = 7;           //D7 лампы  (реле 3)
+const int pumpPin = 47;           //D47 насос  (реле 1)
+const int fanPin = 48;            //D48 вентилятор  (реле 2)
+const int lightPin = 49;          //D49 лампы  (реле 3)
 #define _LCD_TYPE 1               //D20-D21   для работы с I2C дисплеями
+const int button = 22;            //D22 кнопка для управления экраном
 
 
 // значения с датчиков
@@ -34,26 +37,32 @@ int soilMoisturePercent;  // процент влажности почвы
 float humidityPercent;    // влажность воздуха
 float temperature;        // температура воздуха
 
-//======================================================
+
 // config
-bool debugMode = false;  //debug mode - true выводит данные с всех датчиков в порт монитора в читаемом виде; false - вывода нет
+bool debugMode = false;  //debug mode - true выводит данные с всех датчиков и переменные в порт в читаемом виде
 int cycleTime = 1;       //время цикла в секундах
 
-int wateringDuration = 8;    //длительность полива (сек)
-int wateringDelay = 36000;   //задержка полива (сек)   *если полив нужен, но не прошло необходимое время - полив не будет осуществлён  (1 час - 3600 сек, сутки - 86400 сек)
+int wateringDuration = 8;   //длительность полива (сек)
+int wateringDelay = 36000;  //задержка полива (сек)   *если полив нужен, но не прошло необходимое время - полив не будет осуществлён  (1 час - 3600 сек, сутки - 86400 сек)
 
-int ventilationDuration = 600; //длительность проветривания (сек)
-int ventilationDelay = 86400;  //задержка проветривания (сек)   *если проветривание нужно, но не прошло необходимое время - проветривание не будет осуществлёно ()
-//======================================================
+int ventilationDuration = 600;  //длительность проветривания (сек)
+int ventilationDelay = 86400;   //задержка проветривания (сек)   *если проветривание нужно, но не прошло необходимое время - проветривание не будет осуществлёно ()
+
+int lcdLightDuration = 10; //длительность подсветки lcd экрана после нажатия кнопки (сек)
+
 
 // служебные переменные
-bool needWatering;     //необходимость полива
-bool watering;         //полив
+bool needWatering;  //необходимость полива
+bool watering;      //полив
 
 bool needVentilation;  //необходимость проветривания
 bool ventilation;      //проветривание
+bool lcdLight;         //подсветка экрана 
 
-int illuminationPercentOld; //старые значения датчиков
+unsigned long sensorsTimer; // таймер для датчиков
+unsigned long lightTimer;   // таймер для подсветки экрана
+
+int illuminationPercentOld;  //старые значения датчиков
 int soilMoisturePercentOld;
 float humidityPercentOld;
 float temperatureOld;
@@ -71,24 +80,30 @@ void setup() {
   lcd.init();
   lcd.backlight();
 
-    lcd.setCursor(0, 0);
-    lcd.print("BoroFarm v0.13");
-    delay(2000);
-    lcd.setCursor(0, 1);
-    lcd.print("Ilya Borodin (c)");
-    delay(5000);
-    lcd.setCursor(0, 0); 
-    lcd.print("                ");
-    lcd.setCursor(0, 1);
-    lcd.print("                ");
+  lcd.setCursor(0, 0);
+  lcd.print("BoroFarm v0.16");
+  delay(2000);
+  lcd.setCursor(0, 1);
+  lcd.print("Ilya Borodin (c)");
+  delay(5000);
+  lcd.noBacklight();
+  lcd.setCursor(0, 0);
+  lcd.print("                ");
+  lcd.setCursor(0, 1);
+  lcd.print("                ");
 
-  // выключаем все реле
+
   pinMode(fanPin, OUTPUT);
   pinMode(pumpPin, OUTPUT);
-  pinMode(lightPin, OUTPUT); 
+  pinMode(lightPin, OUTPUT);
+  pinMode(button, INPUT);
+
+  // выключаем все реле
   digitalWrite(fanPin, HIGH);
   digitalWrite(pumpPin, HIGH);
   digitalWrite(lightPin, HIGH);
+
+  sensorsTimer = millis();
 }
 
 //======================================================
@@ -96,74 +111,85 @@ void setup() {
 void loop() {
 
 
-  //запись данных с датчика освещенности
-  lightIndicatorValue = analogRead(lightIndicatorPin);
-  illuminationPercent = lightIndicatorValue / 10;
-  //обновление данных на экране
-  if (illuminationPercentOld != illuminationPercent){
-    illuminationPercentOld = illuminationPercent;
-    lcd.setCursor(6, 1);
-    lcd.print("     ");
-    lcd.setCursor(6, 1);
-    lcd.print("i ");
-    lcd.print(illuminationPercent);
-    lcd.print("%");
+  if (millis() - sensorsTimer > cycleTime * 1000) {
+
+    //освещенность
+    lightIndicatorValue = analogRead(lightIndicatorPin);
+    illuminationPercent = lightIndicatorValue / 10;
+    //обновление данных на экране
+    if (illuminationPercentOld != illuminationPercent) {
+      illuminationPercentOld = illuminationPercent;
+      lcd.setCursor(6, 1);
+      lcd.print("     ");
+      lcd.setCursor(6, 1);
+      lcd.print("i ");
+      lcd.print(illuminationPercent);
+      lcd.print("%");
+    }
+
+    //датчик влажночсти почвы
+    soilMoistureValue = analogRead(soilMoisturePin);
+    soilMoisturePercent = (-soilMoistureValue + 480) * 100 / 300;  // 462 - показание датчика на воздухе / 260 - политая земля /
+    //обновление данных на экране
+    if (soilMoisturePercentOld != soilMoisturePercent) {
+      soilMoisturePercentOld = soilMoisturePercent;
+      lcd.setCursor(6, 0);
+      lcd.print("     ");
+      lcd.setCursor(6, 0);
+      lcd.print("S ");
+      lcd.print(soilMoisturePercent);
+      lcd.print("%");
+    }
+
+    //влажность воздуха
+    humidityPercent = dht.readHumidity();
+    if (humidityPercentOld != humidityPercent) {
+      humidityPercentOld = humidityPercent;
+      lcd.setCursor(0, 1);
+      lcd.print("     ");
+      lcd.setCursor(0, 1);
+      lcd.print("H ");
+      lcd.print(round(humidityPercent));
+      lcd.print("%");
+    }
+
+    //температура
+    temperature = dht.readTemperature();
+    if (temperatureOld != temperature) {
+      temperatureOld = temperature;
+      lcd.setCursor(0, 0);
+      lcd.print("     ");
+      lcd.setCursor(0, 0);
+      lcd.print("t ");
+      lcd.print(round(temperature));
+      lcd.print("\337");
+    }
+    //вывод данных в порт
+    if (debugMode) {
+      Serial.print(illuminationPercent);
+      Serial.print(", ");
+      Serial.print(soilMoisturePercent);
+      Serial.print(", ");
+      Serial.print(humidityPercent);
+      Serial.print(", ");
+      Serial.println(temperature);
+    }  
+
+    sensorsTimer = millis();
   }
 
-  //датчик влажночсти почвы
-  soilMoistureValue = analogRead(soilMoisturePin);
-  soilMoisturePercent = (-soilMoistureValue + 480) * 100 / 300;  // 462 - показание датчика на воздухе / 260 - политая земля /
-  //обновление данных на экране
-  if (soilMoisturePercentOld != soilMoisturePercent){
-    soilMoisturePercentOld = soilMoisturePercent;
-    lcd.setCursor(6, 0);
-    lcd.print("     ");
-    lcd.setCursor(6, 0);
-    lcd.print("S ");
-    lcd.print(soilMoisturePercent);
-    lcd.print("%");
+
+  // подсветка экрана 
+  if (digitalRead(button) == 1){
+    lcdLight = true;
+    lcd.backlight();
+    lightTimer = millis();
+  };
+
+  if (lcdLight && millis() - lightTimer >= lcdLightDuration * 1000) {
+    lcd.noBacklight();
+    lcdLight = false;
   }
 
-  //влажность воздуха
-  humidityPercent = dht.readHumidity();
-  if (humidityPercentOld != humidityPercent){
-    humidityPercentOld = humidityPercent;
-    lcd.setCursor(0, 1);
-    lcd.print("     ");
-    lcd.setCursor(0, 1);
-    lcd.print("H ");
-    lcd.print(round(humidityPercent));
-    lcd.print("%");
-  }
-
-  //температура
-  temperature = dht.readTemperature();
-  if (temperatureOld != temperature){
-    temperatureOld = temperature;
-    lcd.setCursor(0, 0);
-    lcd.print("     ");
-    lcd.setCursor(0, 0);
-    lcd.print("t ");
-    lcd.print(round(temperature));
-    lcd.print("\337");
-  }
-
-  //вывод данных на монитор
-  if (debugMode) {
-    Serial.println("");
-  } else {
-
-    Serial.print(illuminationPercent);
-    Serial.print(", ");
-    Serial.print(soilMoisturePercent);
-    Serial.print(", ");
-    Serial.print(humidityPercent);
-    Serial.print(", ");
-    Serial.println(temperature);
-  }
-
-
-
-  delay(cycleTime * 1000);  //задержка цикла
+  
 }
-  щя
